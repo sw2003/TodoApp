43fc7fb2221ddbf7aebcca1450b044dfe9da3f55
Sam <samwentupp@outlook.com>
2023-06-11 10:54:53 -0700

    html css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TodoApp</h1>
+      <form>
+        <div>
+          <label htmlFor="input_name">Add Name</label>
+          <input type="text" id="input_name" class="input__lg"></input>
+        </div>
+        <div>
+          <label htmlFor="input_date">Add Date</label>
+          <input type="text" id="input_date" class="input__lg"></input>
+        </div>
+        <div>
+          <label htmlFor="input_desc">Add Description</label>
+          <textarea id="input_desc" class="input_desc input__lg"></textarea>
+        </div>
+      </form>
+      <ul>
+        <li>
+          <div class = "todo-name">
+            <h2>3/13/2023</h2>
+            <h2>Sleep</h2>
+          </div>
+          <div class ="btn-group">
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </li>
+        <li>
+          <div class = "todo-name">
+            <h2>4/13/2023</h2>
+            <h2>Work</h2>
+          </div>
+          <div class = "btn-group">
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </li>
+      </ul>
     </div>
   );
 }
